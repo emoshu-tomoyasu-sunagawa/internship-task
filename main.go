5b494c29a3ccfe8e0abc64137e3b3b599afbf8c7
Tomoyasu Sunagawa <tomoyasu.sunagawa@emoshu.co.jp>
2023-02-20 16:36:44 +0900

    モデル名をテーブル名に揃えてMemberに変更した

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"gorm.io/driver/mysql"
@@ -10,7 +9,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Employee struct {
+type Member struct {
 	gorm.Model
 	Id               int    `json:"id"`
 	No               string `json:"no"`
@@ -25,53 +24,34 @@ type Employee struct {
 	Status           string `json:"status"`
 }
 
-type Employees struct {
-	Employees []Employee `json:"employee"`
+type Members struct {
+	Members []Member `json:"member"`
 }
 
 func main() {
 	e := echo.New()
 
-	fmt.Printf("Air聴いてる？")
-	// dsn := "root:password@tcp(127.0.0.1:3306)/emonavi_db?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	panic("Failed to connect DB")
-	// }
-
-	// ルーティングへ飛ばす
-	// initRouting(e)
-
 	e.GET("/", hello)
-	e.POST("/employee", createEmployee)
+	e.POST("/member", createMember)
 
 	e.Start(":3000")
-	e.Logger.Fatal(e.Start(":3000"))
 }
-
-// ルーティングの一覧
-// func initRouting(e *echo.Echo) {
-// 	e.GET("/", hello)
-// 	e.POST("/employee", createEmployee)
-// }
 
 func hello(c echo.Context) error {
 	db := DBConnection()
-	var member = Employee{No: "020", ProfileImg: "https://emoshu.co.jp", FullName: "John Doe"}
+	var member = Member{No: "020", ProfileImg: "https://emoshu.co.jp", FullName: "John Doe"}
 	db.Create(&member)
 
 	return c.String(http.StatusOK, "新しい従業員を追加しました！")
 }
 
-func createEmployee(c echo.Context) error {
+func createMember(c echo.Context) error {
 	db := DBConnection()
 	no := c.FormValue("no")
 	profile_img := c.FormValue("profile_img")
 	full_name := c.FormValue("full_name")
 
-	fmt.Printf(profile_img)
-
-	var member = Employee{No: no, ProfileImg: profile_img, FullName: full_name}
+	var member = Member{No: no, ProfileImg: profile_img, FullName: full_name}
 	db.Create(&member)
 
 	return c.String(http.StatusOK, full_name)

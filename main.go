6ae22ebcb385f38406c9a4e50bce0ec6eae144a5
Tomoyasu Sunagawa <tomoyasu.sunagawa@emoshu.co.jp>
2023-02-28 14:48:09 +0900

    社員の一覧を取得でDBに接続できなかったときにエラーメッセージを返す

--- main.go
+++ main.go
@@ -33,85 +33,90 @@ type ErrorMessage struct {
 func main() {
 	e := echo.New()
 
-	e.POST("/member", createMember)        // 社員の新規登録
-	e.GET("/members", getAllMembers)       // 社員の一覧取得
-	e.GET("/members/:id", getMember)       // 社員の詳細情報取得
-	e.PUT("/members/:id", updateMember)    // 社員の情報を更新する
-	e.DELETE("/members/:id", deleteMember) // 社員情報を削除する
+	// e.POST("/member", createMember)        // 社員の新規登録
+	e.GET("/members", getAllMembers) // 社員の一覧取得
+	// e.GET("/members/:id", getMember)       // 社員の詳細情報取得
+	// e.PUT("/members/:id", updateMember)    // 社員の情報を更新する
+	// e.DELETE("/members/:id", deleteMember) // 社員情報を削除する
 	e.Start(":3000")
 }
 
 // 社員の新規登録
-func createMember(c echo.Context) error {
-	var member Member
-	err := c.Bind(&member)
-	if err != nil {
-		return c.String(http.StatusBadRequest, "It's a bad request!")
-	}
+// func createMember(c echo.Context) error {
+// 	var member Member
+// 	err := c.Bind(&member)
+// 	if err != nil {
+// 		return c.String(http.StatusBadRequest, "It's a bad request!")
+// 	}
 
-	db := DBConnection()
-	db.Create(&member)
+// 	db := DBConnection()
+// 	db.Create(&member)
 
-	return c.String(http.StatusOK, member.FullName+"さんの社員情報を登録しました")
-}
+// 	return c.String(http.StatusOK, member.FullName+"さんの社員情報を登録しました")
+// }
 
 // 社員の一覧取得
 func getAllMembers(c echo.Context) error {
 	var members []Member
-	db := DBConnection()
+	db, err := DBConnection()
+
+	if err != nil {
+		ErrorMessage := ErrorMessage{Status: 500, Message: "DBとの接続に失敗しました。"}
+		return c.JSON(http.StatusOK, ErrorMessage)
+	}
 	db.Find(&members)
 
 	return c.JSON(http.StatusOK, members)
 }
 
 // 社員の詳細情報取得
-func getMember(c echo.Context) error {
-	var member Member
-	id := c.Param("id")
-	db := DBConnection()
+// func getMember(c echo.Context) error {
+// 	var member Member
+// 	id := c.Param("id")
+// 	db := DBConnection()
 
-	if err := db.First(&member, id).Error; err != nil {
-		ErrorMessage := ErrorMessage{Status: 404, Message: "有効なID番号ではありません。"}
-		return c.JSON(http.StatusBadRequest, ErrorMessage)
-	}
+// 	if err := db.First(&member, id).Error; err != nil {
+// 		ErrorMessage := ErrorMessage{Status: 404, Message: "有効なID番号ではありません。"}
+// 		return c.JSON(http.StatusBadRequest, ErrorMessage)
+// 	}
 
-	return c.JSON(http.StatusOK, member)
-}
+// 	return c.JSON(http.StatusOK, member)
+// }
 
 // 社員の情報を更新する
-func updateMember(c echo.Context) error {
-	var member Member
-	id := c.Param("id")
-	db := DBConnection()
-	db.First(&member, id)
+// func updateMember(c echo.Context) error {
+// 	var member Member
+// 	id := c.Param("id")
+// 	db := DBConnection()
+// 	db.First(&member, id)
 
-	// ID番号に応じたユーザーがDBから取得できているかの確認
-	if member.Id == 0 {
-		return c.JSON(http.StatusBadRequest, "There is NO user!")
-	}
+// 	// ID番号に応じたユーザーがDBから取得できているかの確認
+// 	if member.Id == 0 {
+// 		return c.JSON(http.StatusBadRequest, "There is NO user!")
+// 	}
 
-	if err := c.Bind(&member); err != nil {
-		return err
-	}
+// 	if err := c.Bind(&member); err != nil {
+// 		return err
+// 	}
 
-	db.Save(&member)
-	return c.JSON(http.StatusOK, member)
-}
+// 	db.Save(&member)
+// 	return c.JSON(http.StatusOK, member)
+// }
 
 // 社員情報を削除する
-func deleteMember(c echo.Context) error {
-	var member Member
-	id := c.Param("id")
-	db := DBConnection()
-	db.First(&member, id)
+// func deleteMember(c echo.Context) error {
+// 	var member Member
+// 	id := c.Param("id")
+// 	db := DBConnection()
+// 	db.First(&member, id)
 
-	db.Delete(&Member{}, id)
+// 	db.Delete(&Member{}, id)
 
-	return c.JSON(http.StatusOK, member.Id)
-}
+// 	return c.JSON(http.StatusOK, member.Id)
+// }
 
-func DBConnection() *gorm.DB {
-	err := godotenv.Load("./.env")
+func DBConnection() (db *gorm.DB, err error) {
+	err = godotenv.Load("./.env")
 	if err != nil {
 		panic("Error loading .env file")
 	}
@@ -122,10 +127,13 @@ func DBConnection() *gorm.DB {
 	database := os.Getenv("MYSQL_DATABASE")
 	dsn := user + ":" + password + "@tcp(" + container_name + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect DB")
-	}
+	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	return db
+	// // db = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	ErrorMessage := ErrorMessage{Status: 404, Message: "エラ〜"}
+	// 	return c.JSON(http.StatusOK, ErrorMessage)
+	// }
+
+	// return db
 }
